77c5b50d14fb4820f2abc18ec0e20eee9067504a
regaldisclaimer <regaldisclaimer@gmail.com>
2015-10-24 01:05:58 -0400

    get blocks start

--- index.js
+++ index.js
@@ -2,29 +2,36 @@
 var ref = new Firebase("https://crowdfall.firebaseio.com");
 
 var userRef;
-var auth
+var auth;
+var leftRef;
+var rightRef;
 
 var team;
+var currBlock;
 
 //onAuth watch
 ref.onAuth(function(authData) {
 	if (authData) {
 		console.log("Authenticated with uid:", authData.uid);
-		auth = authData;
-		userRef = new Firebase("https://crowdfall.firebaseio.com/users/"+authData.uid);
-		assignTeam();
-
+		setVarsOnAuth(authData);
 	} else {
 		console.log("Unauthenticated");
 	}
 });
 
-
+function setVarsOnAuth(authData) {
+	auth = authData;
+	userRef = new Firebase("https://crowdfall.firebaseio.com/users/"+authData.uid);
+	assignTeam();
+	leftRef = ref.child("leftBlocks");
+	rightRef = ref.child("rightBlocks");
+}
 
 function fbAuth() {
 	ref.authWithOAuthPopup("facebook", function(error, authData) {
 	  if (error) {
 	    console.log("Login Failed!", error);
+	    setVarsOnAuth(authData);
 	  } else {
 	    console.log("Authenticated successfully with payload:", authData);
 	  }
@@ -34,9 +41,9 @@ function fbAuth() {
 //new users will be assigned to a new team randomly
 function assignTeam() {
 	//check if new user
-	var teamRef = userRef.child('team');
+	var teamRef = userRef.child("team");
 
-	teamRef.on('value', function(snap) {
+	teamRef.on("value", function(snap) {
 		if (snap) {
 			//not a new user
 			team = snap.val();
@@ -46,13 +53,23 @@ function assignTeam() {
 			var roll = Math.random();
 
 			if (roll < 0.5) {
-				team = "l"
+				team = "l";
 			} else {
-				team = "r"
+				team = "r";
 			}
 
 			//store value on firebase
 			teamRef.set(team);
 		}
 	});
+}
+
+//fetches commands starting from last block
+function getLeftBlocks() {
+
+}
+
+//fetches commands starting from last block
+function getRightBlocks() {
+
 }
