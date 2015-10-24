167cf319dd5163768c486b862b182f7fbc7072aa
regaldisclaimer <regaldisclaimer@gmail.com>
2015-10-24 00:38:34 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var ref = new Firebase("https://crowdfall.firebaseio.com");
+ref.authWithOAuthPopup("facebook", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
+
+
